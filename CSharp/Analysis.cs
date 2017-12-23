844f856dc0f2cc8eca9cc7d9690f56484292b3fc
IAIOO <IAIOO@users.noreply.github.com>
2017-12-23 22:58:08 +0800

    fix bug

---
+++ CSharp/Analysis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Analysis
+    {
+        public string DataAnalysis(DataObject dataObject)
+        {
+            Dictionary<string, int> programLanguage = dataObject.AllInfoObject.ProgramLanguage;
+            var data =
+            from language in programLanguage
+            orderby language.Value descending
+            select language;
+            int maxAsked = data.First().Value;
+            string maxLanguage = data.First().Key;
+            return maxLanguage + maxAsked;
+        }
+
+    }
+}
