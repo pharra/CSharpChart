9995b962e5cdb4b708e3e33c7a69ef7ec0079241
pharra <940530348@qq.com>
2017-12-24 15:56:04 +0800

    a

--- CSharp/DataAnalysis.cs
+++ CSharp/DataAnalysis.cs
@@ -19,7 +19,7 @@ namespace CSharp
             string maxLanguage = data.First().Key;
             string result = maxLanguage + "语言的需求量最大，有" + maxCount + "个岗位可供选择。";
             string advise = "建议学习者可以倾向于学习" + maxLanguage + "以便日后求职时能够有较多选择；而求职者则可以加深对" + maxLanguage +"的研究，以便在激烈的竞争中脱颖而出。";
-            return result + advise;
+            return result + "\n" + advise;
         }
 
         public string CompanyAnalysis(DataObject dataObject, string companyName)
@@ -35,7 +35,7 @@ namespace CSharp
             string maxTecStack = position.First().Key;
             string result = companyName + "对" + maxTecStack + "岗位的需求最大，共有" + maxCount + "个需求。";
             string advise = "建议对" + companyName + "感兴趣的求职者多了解" + maxTecStack + "方面的知识，以便提高进入心仪公司的概率。";
-            return result + advise;
+            return result +"\n" + advise;
         }
 
         public string AddressAnalysis(DataObject dataObject, string addressName)
@@ -54,7 +54,7 @@ namespace CSharp
             string needestCompany = addressInfo.ElementAt(maxCount).Key;
             string result =  "在" + addressName + "，" + needestCompany + "公司对人才的需求最大，共有" + maxCount + "个岗位存在需求";
             string advise = "对于家在" + addressName + "的求职者，建议前往" + needestCompany + "进行求职，该公司人才需求相较其他公司较大，求职成功性较高。";
-            return result + advise;
+            return result + "\n" + advise;
         }
     }
 }
