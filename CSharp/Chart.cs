f3a4cba0d95a1ce1508cefde0ac5edbfc98a97cc
iss2016302580133 <987761617@qq.com>
2017-12-24 14:54:03 +0800

    chart

--- CSharp/Chart.cs
+++ CSharp/Chart.cs
@@ -8,21 +8,68 @@ namespace CSharp
 {
     class Chart
     {
-        public void CsChart(DataObject dataObject)
+        public Dictionary<List<string>,List<int>> LanguageChart(DataObject dataObject)
         {
-            Dictionary<string, int> programLanguage = dataObject.AllInfoObject.ProgramLanguage;
-            xData = new List<string>();
-           yData = new List<int>();
-            foreach(var data in programLanguage)
+            Dictionary<List<string>, List<int>> pair = new Dictionary<List<string>, List<int>>();
+            List<string> xData = new List<string>();
+            List<int> yData = new List<int>();
+            foreach(var data in dataObject.AllInfoObject.ProgramLanguage)
             {
                 string language = data.Key;
                 xData.Add(language);
                 int num = data.Value;
                 yData.Add(num);
             }
+            pair.Add(xData, yData);
+            return pair;
         }
-        public List<string> xData { set; get; }
-        public List<int> yData{ set; get; }
 
+        public Dictionary<List<string>, List<int>> StackChart(DataObject dataObject)
+        {
+            Dictionary<List<string>, List<int>> pair = new Dictionary<List<string>, List<int>>();
+            List<string> xData = new List<string>();
+            List<int> yData = new List<int>();
+            foreach (var data in dataObject.AllInfoObject.TechnologyStack)
+            {
+                string technologyStack = data.Key;
+                xData.Add(technologyStack);
+                int num = data.Value;
+                yData.Add(num);
+            }
+            pair.Add(xData, yData);
+            return pair;
+        }
+
+        public Dictionary<List<string>, List<int>> JobChart(DataObject dataObject)
+        {
+            Dictionary<List<string>, List<int>> pair = new Dictionary<List<string>, List<int>>();
+            List<string> xData = new List<string>();
+            List<int> yData = new List<int>();
+            foreach (var data in dataObject.AllInfoObject.Job)
+            {
+                string job = data.Key;
+                xData.Add(job);
+                int num = data.Value;
+                yData.Add(num);
+            }
+            pair.Add(xData, yData);
+            return pair;
+        }
+
+        public Dictionary<List<string>, List<int>> AddressChart(DataObject dataObject)
+        {
+            Dictionary<List<string>, List<int>> pair = new Dictionary<List<string>, List<int>>();
+            List<string> xData = new List<string>();
+            List<int> yData = new List<int>();
+            foreach (var data in dataObject.AllInfoObject.Address)
+            {
+                string address = data.Key;
+                xData.Add(address);
+                int num = data.Value;
+                yData.Add(num);
+            }
+            pair.Add(xData, yData);
+            return pair;
+        }
     }
 }
