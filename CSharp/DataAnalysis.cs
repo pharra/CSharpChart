eb053bf57aab3a754269cdd115fd2864e2343138
IAIOO <IAIOO@users.noreply.github.com>
2017-12-24 17:09:27 +0800

    fix add

--- CSharp/DataAnalysis.cs
+++ CSharp/DataAnalysis.cs
@@ -34,16 +34,16 @@ namespace CSharp
             from yourJob in job
             orderby yourJob.Value descending
             select yourJob;
-            int maxCount3 = data1.First().Value;
-            string maxJob = data1.First().Key;
+            int maxCount3 = data3.First().Value;
+            string maxJob = data3.First().Key;
             string result3 = maxJob + "岗位的需求量最大，有" + maxCount3 + "个岗位可供选择。";
             string advise3 = "建议学习者可以倾向于学习" + maxJob + "方面的知识，以便日后求职时能够有较多选择；而求职者则可以加深对" + maxJob + "的研究，以便在激烈的竞争中脱颖而出。";
             var data4 =
             from theAddress in address
             orderby theAddress.Value descending
             select theAddress;
-            int maxCount4 = data1.First().Value;
-            string maxAddress = data1.First().Key;
+            int maxCount4 = data4.First().Value;
+            string maxAddress = data4.First().Key;
             string result4 = maxAddress + "的人才需求量最大，有" + maxCount4 + "个岗位急需人才。";
             string advise4 = "建议求职者可以前往" + maxAddress + "进行求职";
             return result1 + "\n" + advise1 + "\n" + result2 + "\n" + advise2 + "\n" + result3 + "\n" + advise3 + "\n" + result4 + "\n" + advise4;
@@ -74,16 +74,16 @@ namespace CSharp
                 from yourJob in companyInfo.Job
                 orderby yourJob.Value descending
                 select yourJob;
-            int maxCount3 = data1.First().Value;
-            string maxJob = data1.First().Key;
+            int maxCount3 = data3.First().Value;
+            string maxJob = data3.First().Key;
             string result3 = companyName + "对" + maxJob + "的需求最大，共有" + maxCount3 + "个需求。";
             string advise3 = "建议对" + companyName + "感兴趣的求职者多了解" + maxJob + "方面的知识，以便提高进入心仪公司的概率。";
             var data4 =
                 from address in companyInfo.Address
                 orderby address.Value descending
                 select address;
-            int maxCount4 = data1.First().Value;
-            string maxAddress = data1.First().Key;
+            int maxCount4 = data4.First().Value;
+            string maxAddress = data4.First().Key;
             string result4 = companyName + "在" + maxAddress + "对人才的需求最大，共有" + maxCount4 + "个需求。";
             string advise4 = "建议对" + companyName + "感兴趣的求职者前往" + maxAddress + "，以便提高进入心仪公司的概率。";
             return result1 +"\n" + advise1 + "\n" + result2 + "\n" + advise2 + "\n" + result3 + "\n" + advise3 + "\n" + result4 + "\n" + advise4;
@@ -103,9 +103,31 @@ namespace CSharp
                 }
             }
             string needestCompany = addressInfo.ElementAt(maxCount1).Key;
-            string result =  "在" + addressName + "，" + needestCompany + "公司对人才的需求最大，共有" + maxCount1 + "个岗位存在需求";
-            string advise = "对于家在" + addressName + "的求职者，建议前往" + needestCompany + "进行求职，该公司人才需求相较其他公司较大，求职成功性较高。";
-            return result + "\n" + advise;
+            string result1 =  "在" + addressName + "，" + needestCompany + "公司对人才的需求最大，共有" + maxCount1 + "个岗位存在需求";
+            string advise1 = "对于家在" + addressName + "的求职者，建议前往" + needestCompany + "进行求职，成功性较高。";
+            int maxCount2 = 0;
+            foreach (var company in addressInfo)
+            {
+                if (maxCount2 < company.Value.ProgramLanguage.Values.Sum())
+                {
+                    maxCount2 = company.Value.ProgramLanguage.Values.Sum();
+                }
+            }
+            string maxLanguage = addressInfo.ElementAt(maxCount2).Key;
+            string result2 = "在" + addressName + "，" + needestCompany + "公司对" + maxLanguage + "语言的人才需求最大，共有" + maxCount2 + "个岗位存在需求";
+            string advise2 = "对于家在" + addressName + "的求职者，如果擅长" + maxLanguage + "，则建议前往" + needestCompany + "进行求职，成功性较高。";
+            int maxCount3 = 0;
+            foreach (var company in addressInfo)
+            {
+                if (maxCount3 < company.Value.TechnologyStack.Values.Sum())
+                {
+                    maxCount3 = company.Value.TechnologyStack.Values.Sum();
+                }
+            }
+            string maxStack = addressInfo.ElementAt(maxCount3).Key;
+            string result3 = "在" + addressName + "，" + needestCompany + "公司对" + maxStack + "编程的人才需求最大，共有" + maxCount3 + "个岗位存在需求";
+            string advise3 = "对于家在" + addressName + "的求职者，如果擅长" + maxStack + "方面的编程，则建议前往" + needestCompany + "进行求职，成功性较高。";
+            return result1 + "\n" + advise1 + "\n"+ result2 + "\n" + advise2 + "\n"+result3 + "\n" + advise3;
         }
     }
 }
