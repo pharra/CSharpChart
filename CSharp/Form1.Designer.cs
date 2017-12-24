344800e31c4b4ce2b38d36dc62a1abe2581b7e8d
pharra <940530348@qq.com>
2017-12-24 12:37:45 +0800

    fix wqy's bug

--- CSharp/Form1.Designer.cs
+++ CSharp/Form1.Designer.cs
@@ -46,8 +46,7 @@
             legend1.BackColor = System.Drawing.Color.Transparent;
             legend1.Name = "Legend1";
             this.Language.Legends.Add(legend1);
-            this.Language.Location = new System.Drawing.Point(31, 83);
-            this.Language.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Language.Location = new System.Drawing.Point(58, 251);
             this.Language.Name = "Language";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
@@ -55,43 +54,40 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.Language.Series.Add(series1);
-            this.Language.Size = new System.Drawing.Size(452, 324);
+            this.Language.Size = new System.Drawing.Size(1088, 578);
             this.Language.TabIndex = 0;
             this.Language.Text = "chart1";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.ItemHeight = 15;
+            this.comboBox1.ItemHeight = 24;
             this.comboBox1.Items.AddRange(new object[] {
             "语言分析"});
-            this.comboBox1.Location = new System.Drawing.Point(31, 31);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Location = new System.Drawing.Point(46, 50);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(321, 23);
+            this.comboBox1.Size = new System.Drawing.Size(480, 32);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(381, 31);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(572, 50);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 33);
+            this.button1.Size = new System.Drawing.Size(153, 53);
             this.button1.TabIndex = 2;
             this.button1.Text = "查看";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(559, 438);
+            this.ClientSize = new System.Drawing.Size(1402, 913);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.Language);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.Language)).EndInit();
